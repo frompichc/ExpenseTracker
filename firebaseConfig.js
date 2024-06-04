a43d2dd17d99af39feb973f807ddfb1f854998fb
Fredy Rompich <fredyrompich@gmail.com>
2024-06-04 09:21:21 +0200

    First time sending

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvPpckji4G5C7FjyJNHW_web3LvyxThlo",
+    authDomain: "crud-c7bce.firebaseapp.com",
+    projectId: "crud-c7bce",
+    storageBucket: "crud-c7bce.appspot.com",
+    messagingSenderId: "384849316689",
+    appId: "1:384849316689:web:3c0900ef12c33e8ee9e170",
+    measurementId: "G-94WL35E5WD"
+};
+
+export default firebaseConfig;
--- script.js
+++ script.js
@@ -6,15 +6,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/fireba
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCvPpckji4G5C7FjyJNHW_web3LvyxThlo",
-    authDomain: "crud-c7bce.firebaseapp.com",
-    projectId: "crud-c7bce",
-    storageBucket: "crud-c7bce.appspot.com",
-    messagingSenderId: "384849316689",
-    appId: "1:384849316689:web:3c0900ef12c33e8ee9e170",
-    measurementId: "G-94WL35E5WD"
-};
+import firebaseConfig from "./firebaseConfig.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
